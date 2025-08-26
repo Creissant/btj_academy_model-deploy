17d8c32bf64d2fff38874abcb3f0e6f7b02d536c
Varrel Ng <varrelng@gmail.com>
2025-08-26 21:07:32 +0700

    feat: enhance Titanic prediction API with error handling and integrate it with front-end

--- api/predict/views.py
+++ api/predict/views.py
@@ -1,9 +1,9 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 
 from api.predict.schema import TitanicParams, PredictionResult
 from api.predict.service import PredictTitanic
 
-# Router
+# Router + error handling
 router = APIRouter(prefix="/predict", tags=["Prediction"])
 
 @router.post("/", response_model=PredictionResult)
@@ -13,11 +13,21 @@ def predict_titanic(params: TitanicParams):
     Input: TitanicParams (Pclass, Age, SibSp, Parch, Fare, Sex, Embarked)
     Output: PredictionResult (hasil prediksi + probabilitas + pesan)
     """
-    prediction_service = PredictTitanic(params)
-    pred = prediction_service.predict()
+    try:
+        prediction_service = PredictTitanic(params)
+        pred = prediction_service.predict()
 
-    return PredictionResult(
-        message=pred.get("message", ""),
-        result=pred.get("result"),
-        probability=pred.get("probability")
-    )
+        return PredictionResult(
+            message=pred.get("message", ""),
+            result=pred.get("result"),
+            probability=pred.get("probability")
+        )
+
+    except FileNotFoundError as e:
+        raise HTTPException(status_code=500, detail=str(e)) from e
+    except ValueError as e:
+        raise HTTPException(status_code=400, detail=str(e)) from e
+    except RuntimeError as e:
+        raise HTTPException(status_code=500, detail=str(e)) from e
+    except Exception as e:
+        raise HTTPException(status_code=500, detail="Unexpected error occurred.") from e
